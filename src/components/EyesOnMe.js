031339dd7da622d3a8bd0425531a06c936d223d2
HannaBM12 <Hannabmulugeta@gmail.com>
2021-04-27 23:13:02 -0400

    commit tree af4b9fe5240d44be6dbd7db74243c36c1569ca73

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+// Code EyesOnMe Component Here
+import React from 'react';
+import Keypad from './Keypad'
+
+function EyesOnMe(){
+    return(
+        <div></div>
+
+    )
+}
+
+export default EyesOnMe;
